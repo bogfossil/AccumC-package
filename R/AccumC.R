d381b1ff9349eb5defa81689216d843aae51e617
bogfossil <Jonathan@dhcp-30-177.ldeo.columbia.edu>
2016-07-07 16:39:04 -0400

    add AccumC source
    adding the unbuilt R package

---
+++ R/AccumC.R
@@ -0,0 +1,209 @@
+AccumC <-
+function(x = imv,           # a data frame where name(x) == c("labcode", "depth", "bp", "std", "afbd")
+                   corename=NA,       # the name of the directory and filenames for "corename.dat" and "corename.afbd"
+                   bin_size=NA,       # the tickness of the depth bins in centimeters,
+                   age_bw = NA,       # the size of the age bandwith in years
+                   kernel = "r",      # Kernel shape for PDF smoothing, as in stats::density()
+                   calcurve=intcal13, # can be intcal13 or shcal13 or something else you have
+                   it_power=17,       # log[2] of the number of iterations for Monte Carlo simulation
+                   pct_C=0.48         # fraction of carbon per unit organic matter (translates afbd into carbon)
+                   ){
+  
+data(intcal13)
+data(shcal13)
+
+it <- 2^it_power
+
+##-------------------------------------
+## Start import data
+##-------------------------------------
+ if(sum(c("labcode", "depth", "bp", "std", "afbd") %in% names(imv))==5){
+   datfile <- x
+   }else{ 
+     
+     while(file.exists(paste(corename,"/",corename,".dat",sep=""))==F){
+       corename <- readline("What is the (case sensitive) core name?  \nIf you give the wrong answer, I'll ask you again.  ")
+     }
+     
+     datfile <- read.table(paste(corename,"/",corename,".dat",sep=""),header=T,sep=",")
+   }
+
+datfile <- datfile[order(datfile$labcode),]
+labcodes <- datfile$labcode
+
+##-------------------------------------
+## End import data
+##-------------------------------------
+
+##-------------------------------------
+## Start map dates to cal curve
+##-------------------------------------
+
+allpdfs <- list(NA)
+for(i in 1:length(labcodes)) {
+  bp <- (-5*datfile$std[i]+datfile$bp[i]):(5*datfile$std[i]+datfile$bp[i]) 
+  p <- dnorm(bp,datfile$bp[i],datfile$std[i]) 
+  date <- data.frame(bp,p)
+  
+  cal <- merge(calcurve,date,by='bp')
+  cal <- data.frame(cal$cal,cal$p)
+  cal[,2] <- cal[,2]/sum(cal[,2])
+  allpdfs[[i]] <- cal[order(cal[,1]),]
+}
+
+for(i in 1:length(allpdfs)){
+  names(allpdfs[[i]]) <- c("age",labcodes[i])
+}
+
+
+merged <- allpdfs[[1]]  # Put all the dates onto a single age scale
+for(i in 2:length(allpdfs)){
+  merged <- merge(merged,allpdfs[[i]],all=T)
+}
+merged[is.na(merged)] <- 0 #make NAs in the PDF array into zeroes
+
+
+
+depths <- datfile$depth #These are the depths of each date
+
+if(file.exists(paste(corename,"/",corename,".afbd",sep=""))==T) {
+  bulkDen <- read.table(paste(corename,"/",corename,".afbd",sep=""),header=T,sep=",")
+  datfile$afbd <- approx(bulkDen,xout=depths,rule=2)$y
+} else {}
+
+maxLikeAge <- NA
+for(i in 2:dim(merged)[2]) {
+  maxLikeAge[i-1] <- merged[,1][merged[,i]==max(merged[,i])][1] 
+}
+
+##-------------------------------------
+## End map dates to cal curve
+##-------------------------------------
+
+##-------------------------------------
+## Start merging age PDFs
+##-------------------------------------
+
+# Bin the dates by depth -- 3 (or so) dates per depth bin 
+# (make it an integer, always round down, hence "floor")
+
+  if(is.na(bin_size)==T) {
+    bin_size <- diff(range(depths))/floor(length(labcodes)/3)
+  } else {
+    nbins <- round(diff(range(depths))/as.integer(bin_size))
+    bin_size <- diff(range(depths))/nbins
+  }
+
+depthbins <- 0:(diff(range(depths))/bin_size)*bin_size + min(depths)
+binned <- array(dim=c(nrow(merged),length(depthbins))) #make an empty array with (n-years) rows and (n-depthbins) columns
+binned[,1] <- merged[,1] #make the first column years
+merged <- merged[,-1] #remove the "years" column from the PDF array
+
+for(i in 2:length(depthbins)){  #make a mean PDF for each bin
+  
+  xx <- merged[,depths>=depthbins[i-1]&depths<=depthbins[i]] #assign the bin to a variable
+  
+  if(mode(xx)=="numeric") {binned[,i] <- xx} else {binned[,i] <- apply(xx,1,mean)} 
+  #if there's only one date in a bin, it will be "numeric" else it's a "list"
+  
+}
+binned[is.na(binned)] <- 0
+binned[is.nan(binned)] <- 0
+
+### Combine all the probabilities
+
+prob <- apply(binned[,-1],1,sum)/(ncol(binned)-1)
+prob[is.na(prob)] <- 0
+prob <- data.frame(binned[,1], prob)
+
+##-------------------------------------
+## End merging age PDFs
+##-------------------------------------
+
+
+# Tabluate Metadata
+calibration <- data.frame(labcodes,depths,maxLikeAge)
+meta <- data.frame(bin_size,
+                   length(labcodes), 
+                   round(max(binned[,1])), 
+                   round(bin_size*(ncol(binned)-1)),
+                   corename)
+names(meta) <- c("bin_size","ndates", "age", "depth", "corename")
+
+# Carbon Data
+afbd <- data.frame(maxLikeAge,datfile$afbd) 
+afbd <- afbd[order(maxLikeAge),]
+names(afbd) <- c("depth", "afbd")
+
+
+# -----------------------------------------------------------------------
+# Calculate the Age Model and C-accumulation rates WITH UNCERTAINTIES!!
+# 
+# Start 
+# -----------------------------------------------------------------------
+
+DBS <- meta$bin_size
+
+DPT <- meta$depth
+AGE <- meta$age
+TOP <- round(min(calibration$maxLikeAge))
+
+ndbin <- round((DPT/DBS))
+
+if(is.na(age_bw)==T) {
+  age_bw <- diff(c(TOP,AGE))/length(labcodes)
+} else {
+  age_bw <- as.integer(age_bw)
+}
+
+prob_byone <- density(sample(x = prob[,1], size = it, replace = T, prob = prob[,2]),
+                      cut = 0, bw = age_bw, kernel = kernel)
+prob_byone <- approx(prob_byone, xout=TOP:AGE)
+prob_byone$y[is.na(prob_byone$y)] <- 0
+
+age_test <- array(
+  sample(TOP:AGE, replace=T, size=(ndbin+1)*it, prob=prob_byone$y),
+  dim=c(ndbin+1, it)
+)
+
+max_age <- apply(age_test, 2, min)-apply(age_test, 2, max)
+
+age_test <- apply(age_test, 2, sort)
+age_test <- array(age_test, dim=c(ndbin+1, ncol(age_test)/3, 3) )
+age_test <- apply(age_test, 1:2, median)
+
+ 
+rate_test <- DBS/apply(age_test, 2, diff)
+
+
+AFBD <- matrix(approx(afbd, n=ndbin)$y, byrow=T, nrow = nrow(rate_test), ncol = ncol(rate_test))
+crate_test <-rate_test*AFBD*pct_C*10000
+CRATES <- apply(crate_test, 1, quantile, na.rm=T, probs=c(0.125, 0.25, 0.5, 0.75, 0.875))
+AGES <- apply(apply(age_test, 1:2, median), 1, quantile, na.rm=T, probs=c(0.125, 0.25, 0.5, 0.75, 0.875))
+AGEM <- apply(age_test, 1, median)[-1] - 0.5*diff(apply(age_test, 1, median))
+
+crate_byone <- data.frame(prob_byone$x, 
+                          diff(range(depths))*prob_byone$y*approx(afbd, n=length(prob_byone$x))$y*10000)
+names(crate_byone) <- c("age", "crate")
+
+borders <- apply(apply(age_test, 1:2, median),1,median)
+rates <- data.frame(AGEM,borders[-1], borders[-length(borders)],apply(rate_test, 1, median),CRATES[3,], apply(AFBD, 1, median))
+names(rates) <- c("age","bottom_age","top_age","sed. rate (cm/a)","C accum. rate (g/m^2/a)", "afbd (g/cc)")
+
+# -----------------------------------------------------------------------
+# Calculate the Age Model and C-accumulation rates WITH UNCERTAINTIES!!
+# 
+# End 
+# -----------------------------------------------------------------------
+
+
+## Sew up all the output into a "list" object
+meta$age_bw <- age_bw
+output <- list(meta, calibration, rates, prob_byone, AGES[,-1]-t(apply(AGES, 1, diff)*0.5), age_test, rate_test, crate_test, allpdfs, AGES, CRATES, binned)
+names(output) <- c("metadata","calibrations","rates", "total_prob", "age_mid_quantiles", "age_test", "rate_test", "crate_test", "all_pdfs", "age_quantiles", "rate_quantiles", "binned_pdfs")
+
+class(output) <- "AccumC"
+
+return(output)
+
+}
---
+++ R/plot.AccumC.R
@@ -0,0 +1,246 @@
+plot.AccumC <-
+function(x,                 # an object of class 'AccumC'   
+                        plot=T,            # make a simple plot in the current graphics window
+                        bigplot=T,         # make a pdf showing all the calibrated dates, the age PDFs and the CAR
+                        box_col='gray',    # the color of the bars on the CAR plot
+                        prettytitle=NA,    # The title of the "bigplot"
+                        ratelabel=T,       # add text labels indicating the rate for each bin
+                        log_y_axis=F       # make the rate axis log
+){
+  
+  if(class(x) != "AccumC") {stop('x does not have class "AccumC"', call. = F, domain = NA)} else {}
+  
+##--------------------------------------
+## START Pull data from "AccumC" object
+##--------------------------------------
+  
+  meta <- x$metadata
+  bin_size <- x$metadata$bin_size
+  calibration <- x$calibration
+  rates <- x$rates
+  prob_byone <- x$total_prob
+  age_test <- x$age_test
+  rate_test <- x$rate_test
+  crate_test <- x$crate_test
+  allpdfs <- x$all_pdfs
+  AGES <- x$age_quantiles
+  CRATES <- x$rate_quantiles
+  binned <- x$binned
+  maxLikeAge <- x$calibration$maxLikeAge
+  
+  
+      DBS <- meta$bin_size
+      DPT <- meta$depth
+      AGE <- meta$age
+      TOP <- round(min(calibration$maxLikeAge))
+      ndbin <- round(DPT/DBS)
+      corename <- meta$corename
+      if(is.na(corename) == T & bigplot == T){
+        while(file.exists(paste(corename))==F){
+          corename <- readline("What is the (case sensitive) folder name to save the bigplot?  \nIf you give the wrong answer, I'll ask you again.  ")
+        }
+          }else{}
+      AGEM <- rates$age
+      depths <- calibration$depths
+      depthbins <- 0:(diff(range(depths))/bin_size)*bin_size + min(depths)
+      age_bw <- meta$age_bw
+       
+      
+##-------------------------------------
+## END Pull data from "AccumC" object
+##-------------------------------------
+  
+
+  
+
+##-------------------------------------
+## Start the little plot
+##-------------------------------------
+
+if(plot==T){
+  
+  par(mfrow=c(2,1), oma=c(4,4,4,4), mar=c(0,0,0,0), mgp=c(2, 1, 0))
+  
+  ### Age Model Plot
+  plot(0.001*AGES[3,],DBS*0:(ndbin)+min(depths),
+       xlim=c(0,AGE)*0.001, ylim=c(DPT,0), axes=F,
+       xlab="", ylab="", font.lab=2, t="l", xpd=NA)
+  
+  axis(4)
+  mtext(l=2.5, s=4, "Depth, cm", font=2)
+  
+  segments(0.001*AGES[1,], DBS*0:(ndbin)+min(depths), 
+           0.001*AGES[5,], DBS*0:(ndbin)+min(depths),
+           lty=3, lwd=1, xpd=NA)
+  
+  segments(0.001*AGES[2,], DBS*0:(ndbin)+min(depths), 
+           0.001*AGES[4,], DBS*0:(ndbin)+min(depths),
+           lty=1, lwd=4, col=box_col, xpd=NA)
+  
+  points(0.001*AGES[3,], DBS*0:(ndbin)+min(depths), pch=15, xpd=NA)
+  
+  if(ratelabel == T){
+  text(0.001*AGES[5,-dim(AGES)[2]], DBS*1:ndbin-(DBS*0.5)+0.5*min(depths), 
+       round(rates[,4], 3), cex=0.8, pos = 4)} else {}
+  
+  
+  ### The carbon accumulation rate plot
+  
+  plot(AGEM*0.001, CRATES[3,], t="n",
+       xlim=c(0,AGE)*0.001, ylim=range(CRATES[2:4,]), 
+       axes=F, xlab="", ylab="", font.lab=2,
+       log = ifelse(log_y_axis==T,"y",""))
+  axis(2)
+  mtext(l=2.5, s=2, "gC/m^2/a", font=2)
+  axis(1)
+  mtext(l=2.5, s=1, "Age, cal. ka", font=2)
+  segments(0.001*AGEM, CRATES[1,], 0.001*AGEM, CRATES[5,], lwd=1, lty=3, xpd=NA)
+  rect(0.001*AGES[3,-dim(AGES)[2]], c(CRATES[2,]), 0.001*AGES[3,-1], CRATES[4,], 
+       col=box_col, xpd = NA)
+  # points(0.001*AGEM,CRATES[3,], pch=15)
+  segments(0.001*AGES[3,-dim(AGES)[2]], c(CRATES[3,]), 0.001*AGES[3,-1], CRATES[3,], lwd=2)
+  
+  
+  
+} else {}
+
+
+##-------------------------------------
+## End the little plot
+##-------------------------------------
+
+##-------------------------------------
+## Start the BIG plot
+##-------------------------------------
+
+if(ndbin >= 100) {warning("Too many depth bins for bigplot. I set bigplot = FALSE", call. = F);
+  bigplot = F} else {}
+      
+      
+if(bigplot==T){
+  
+  
+  ### Open a new pdf document  
+  
+  pdf(paste(corename,'/',corename,'.pdf',sep=""),w=6.5,h=9); par(oma=c(2,2,6,2))
+  
+  ### Set the layout for the page of plots
+  
+  layout(
+    mat=array(c(1:(ncol(binned)-1)*0+ncol(binned),ncol(binned)+4,ncol(binned)+4,ncol(binned)+4,1:(ncol(binned)-1),ncol(binned)+1,ncol(binned)+2,ncol(binned)+3),
+              dim=c(ncol(binned)+2,2)
+    ),
+    widths=c(1.25,3),
+    heights=c(rep((1/(ncol(binned)-1)),(ncol(binned)-1)),0.3, 0.6, 0.6)
+  )
+  
+  
+  ### Plot PDFs for each depth bin
+  
+  for(i in 2:(ncol(binned))){
+    par(mar=c(0,5,0,4)); plot(binned[,1],binned[,i],t='l',axes=F,ylab='',xlim=c(0,AGE)); box()
+    mtext(s=2,l=1,paste(i-1),las=1)
+  }
+  
+  
+  title(outer=T,line=2,cex.main=2.5,
+        main=ifelse(is.na(prettytitle),
+                    paste(substr(toupper(corename),start=1,stop=1),substring(corename,first=2),sep=''),
+                    prettytitle)
+  )
+  
+  ### Plot all the calibrated dates  
+  
+  par(mar=c(0,4,0,0))
+  plot(maxLikeAge*0.001,depths,ylim=rev(range(depthbins)),yaxs='i',
+       ylab='Depth, cm',xlab='Age, cal. ka',font.lab=2,
+       xpd=NA,cex.lab=1.2)
+  abline(h=depthbins,lty=3)
+  legend("topright", legend = "A", text.font = 2, cex = 2, bty = "n")
+  
+  #  par(mar=c(3.5,5,3,1));
+  
+  
+  ### Lower Portion
+  
+  par(mar=c(0,5,3,4))
+  
+  ### combined probability plot
+  
+  plot(prob_byone$x*0.001, prob_byone$y, xlim=c(0, AGE)*0.001, axes=F, t="l", xlab="", ylab="")
+  axis(2)
+  mtext(l=2.5, s=2, "Probability", font=2)
+  axis(3,at=0:30*2, line = 0.5)
+  legend("topright", legend = "C", text.font = 2, cex = 2, bty = "n")
+  
+  ### Age Model Plot
+  plot(0.001*AGES[3,],DBS*0:(ndbin)+min(depths),
+       xlim=c(0,AGE)*0.001, ylim=c(DPT,0), axes=F,
+       xlab="", ylab="", font.lab=2, t="l", xpd=NA)
+  
+  axis(4)
+  mtext(l=2.5, s=4, "Depth, cm", font=2)
+  
+  segments(0.001*AGES[1,], DBS*0:(ndbin)+min(depths), 
+           0.001*AGES[5,], DBS*0:(ndbin)+min(depths),
+           lty=3, lwd=1, xpd=NA)
+  
+  segments(0.001*AGES[2,], DBS*0:(ndbin)+min(depths), 
+           0.001*AGES[4,], DBS*0:(ndbin)+min(depths),
+           lty=1, lwd=4, col=box_col, xpd=NA)
+  
+  points(0.001*AGES[3,], DBS*0:(ndbin)+min(depths), pch=15, xpd=NA)
+  
+  text(0.001*AGES[5,-dim(AGES)[2]], DBS*1:ndbin-(DBS*0.5)+0.5*min(depths), 
+       round(rates[,4], 3), cex=0.8, pos = 4)
+  
+  legend("topright", legend = "D", text.font = 2, cex = 2, bty = "n")
+  
+  ### The carbon accumulation rate plot
+  
+  par(mar=c(4,5,0,4))
+  plot(AGEM*0.001, CRATES[3,], t="n",
+       xlim=c(0,AGE)*0.001, ylim=range(CRATES[2:4,]), 
+       axes=F, xlab="", ylab="", font.lab=2,
+       log = ifelse(log_y_axis==T,"y",""))
+  axis(2)
+  mtext(l=2.5, s=2, "gC/m^2/a", font=2)
+  axis(1,at=0:30*2)
+  mtext(l=2.5, s=1, "Age, cal. ka", font=2)
+  segments(0.001*AGEM, CRATES[1,], 0.001*AGEM, CRATES[5,], lwd=1, lty=3, xpd=NA)
+  rect(0.001*AGES[3,-dim(AGES)[2]], c(CRATES[2,]), 0.001*AGES[3,-1], CRATES[4,], 
+       col=box_col, xpd = NA)
+  # points(0.001*AGEM,CRATES[3,], pch=15)
+  segments(0.001*AGES[3,-dim(AGES)[2]], c(CRATES[3,]), 0.001*AGES[3,-1], CRATES[3,], lwd=2)
+  legend("topright", legend = "E", text.font = 2, cex = 2, bty = "n")
+  
+  
+  
+  ### Metadata box  
+  
+  
+  par(mar=c(4.5,4,6,0)); 
+  plot(0:1, 0:1 ,axes=F,xlab='',ylab='', pch=""); box()
+  
+  text(rep(1, 6), 6:1/7, round(c(DPT, min(AGES[3,]), max(AGES[3,]), meta$ndates, DBS, age_bw)),
+       adj=1, font=2)
+  text(rep(0, 6), 6:1/7, paste(c("Thickness (cm)", "Top Age", "Bottom Age", "n Dates", "Bin Size (cm)", "Age BW (yr)"),
+                               ': ',sep=''), adj=0, font=3)
+  
+  legend("topright", legend = "F", text.font = 2, cex = 2, bty = "n")
+  
+  mtext("B", side = 3, line = -2, outer = T, adj = 0.9, cex = 1.5, font = 2)
+  
+  
+  
+  dev.off()
+  
+} else {}
+
+##-------------------------------------
+## End the BIG plot
+##-------------------------------------
+
+      
+      
+}
