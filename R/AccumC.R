d019cce79f3f1e38146b65e1919b4c694698a8bb
bogfossil <bogfossil@users.noreply.github.com>
2017-03-14 16:16:28 -0400

    Update AccumC.R

--- R/AccumC.R
+++ R/AccumC.R
@@ -195,11 +195,11 @@ rate_test <- DBS/apply(age_test, 2, diff)
 rate_test <- rate_test[,apply(is.finite(rate_test), 2, sum)==nrow(rate_test)]       ## remove any tests that include infinite sed rates
 # rate_test <- rate_test*(1/median(as.numeric(rate_test)))*(diff(range(calibration$depths))/diff(range(calibration$maxLikeAge)))
 
-#cDens <- density(log(rate_test))
-#cMode <- cDens$x[which.max(cDens$y)]
+cDens <- density(log(rate_test))
+cMode <- cDens$x[which.max(cDens$y)]
 #fudge <- log(meta$depth / diff(range(maxLikeAge))) / cMode
 #rate_test <- exp(log(rate_test) * fudge)
-rate_test <- exp((log(rate_test) - mean(log(rate_test))) + log(meta$depth / diff(range(maxLikeAge))))
+rate_test <- exp((log(rate_test) - cMode) + log(meta$depth / diff(range(maxLikeAge))))
 
 ##-------------------------------------
 ## End get rid of unreasonable rates
